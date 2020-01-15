91377836188cc968c26b8928bb2d5d784f0e141d
plbakol2 <52651964+plbakol2@users.noreply.github.com>
2020-01-15 17:43:34 +0100

    Update sensors.py

--- Server/sensors.py
+++ Server/sensors.py
@@ -1,6 +1,6 @@
 import Adafruit_DHT
 import RPi.GPIO as GPIO
-import time
+import datetime
 
 
 # This code can be useful for standalone run but for our purpose it should return data
@@ -37,57 +37,87 @@ import time
 
 # you can do changes on other branch and create PR then review will be much easier for me :D
 
+class SensorAPI:
+    """
+    Class to measure temperature, humidity and distance from sensor to opstacle.
+    Designed specialy for raspberry pi.
+    """
 
-def getTempHumi(DHT11_pin=4, sensor=Adafruit_DHT.DHT11):
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
-    
-    if humidity is not None and temperature is not None:
-        return {"data": {"Temp": '{0:0.1f}*C'.format(temperature), "Humidity": '{0:0.1f}%'.format(humidity)}}
-    else:
-        return {"data": {"Temp": None, "Humidity": None}}
+    self.temperature = ""
+    self.humidity = ""
+    self.distance = ""
+    self.sensor_table = {"Temperature and Humindity": {"status": "active"},
+                         "HC SR04": {"status": "active"}}
 
+    def initialize_all_sensors(self):
+        self.hcSr04_Init()
 
-def hcSr04Init(TRIG=21, ECHO=20):
-    # BCM numbering scheme
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    # Set pins
-    TRIG = 21
-    ECHO = 20
-    # Set direction
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+    def getTempHumi(DHT11_pin=4, sensor=Adafruit_DHT.DHT11):
+        """
+        Get the data from sensor od Temperature and Humidity
+        :param sensor: insert pin where is pluged in your sensor and type of sensoe
+        DHT 11 or 22
+        :return:
+        Data with Temperature and Humidity
+        """
 
-
-def getDistance(pinTrig=21, pinEcho=20):
-    # Pulse to start measure with HC-SR04
-    GPIO.output(pinTrig, True)
-    time.sleep(0.00001)
-    GPIO.output(pinTrig, False)
-    # Wait for HIGH on ECHO
-    while GPIO.input(pinEcho) == 0:
-        pulse_start = time.time()
-    # wait for LOW again
-    while GPIO.input(pinEcho) == 1:
-        pulse_end = time.time()
-    signalDelay = pulse_end - pulse_start
-    # divider for uS to  s
-    constDivider = 1000000 / 58
-    distance = int(signalDelay * constDivider)
-    return distance
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
+        if humidity is not None and temperature is not None:
+            return {"data": {"Temp": '{0:0.1f}*C'.format(temperature), "Humidity": '{0:0.1f}%'.format(humidity)}}
+            self.humidity = humidity
+            self.temperature = temperature
+        else:
+            return {"Temp": None, "Humidity": None }
 
 
-def main():
-    HC_SR04_init()
-    print(GetDistance())
-    print(GetTempHumi())
-    GPIO.cleanup()
+    def hcSr04_Init(TRIG=21, ECHO=20):
+        """
+        Init the HCSR04 sensor
+        """
+        # BCM numbering scheme
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        # Set direction
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
+
+
+    def getDistance_Of_HcSr04Init(pinTrig=21, pinEcho=20):
+        """
+        Function to get the distance form opstacle to our sensor
+        :param pinEcho: pin to Trigered, pin to get Data Echo
+        :return: Distance from opstacle
+        """
+        # Pulse to start measure with HC-SR04
+        GPIO.output(pinTrig, True)
+        time.sleep(0.00001)
+        GPIO.output(pinTrig, False)
+        # Wait for HIGH on ECHO
+        while GPIO.input(pinEcho) == 0:
+            pulse_start = time.time()
+        # wait for LOW again
+        while GPIO.input(pinEcho) == 1:
+            pulse_end = time.time()
+        signalDelay = pulse_end - pulse_start
+        # divider for uS to  s
+        constDivider = 1000000 / 58
+        distance = int(signalDelay * constDivider)
+        return {"Distance": distance}
+
+    def getData(self):
+        return {"data":{self.getDistance_Of_HcSr04Init(),self.getTempHumi(),{"Time" : datetime.datetime.now()}}}
+
+    def main():
+        try:
+            self.hcSr04_Init()
+            print(self.getData())
+            GPIO.cleanup()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            GPIO.cleanup()
+
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        GPIO.cleanup()
+    SensorAPI.main()
