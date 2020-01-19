06001e151ba3ab768c2150f01a58c88d93698bc7
Mateusz Bąk <mateusz.bak@x-kom.pl>
2020-01-19 08:34:26 +0100

    Init xcode project

---
+++ IoT/IoT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IoT
+//
+//  Created by Mateusz Bąk on 1/19/20.
+//  Copyright © 2020 Mateusz Bąk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
