f590f057caa5bc911774e4681ac363692af7c86e
Meerchant <kkuuba.k1@gmail.com>
2020-01-31 18:20:45 +0100

    quick fix

--- Server/sensors.py
+++ Server/sensors.py
@@ -1,6 +1,5 @@
 import Adafruit_DHT
 import RPi.GPIO as GPIO
-import datetime
 import time
 
 
@@ -11,36 +10,33 @@ class SensorAPI:
     """
 
     def __init__(self):
-        self.initialize_all_sensors()
-        self.temperature = ""
-        self.humidity = ""
-        self.distance = ""
-        self.sensor_table = {"Temperature and Humindity": {"status": "active"},
-                             "HC SR04": {"status": "active"}}
-        self.initialize_all_sensors()
+        self.response_data = dict()
 
     def initialize_all_sensors(self):
-        self.hcSr04_Init()
-
-    def active_sensors(self):
-        return self.sensor_table
-
-    def getTempHumi(self, DHT11_pin=4, sensor=Adafruit_DHT.DHT11):
-        """
-        Get the data from sensor od Temperature and Humidity
-
-        :param sensor: insert pin where is pluged in your sensor and type of sensor DHT 11 or 22
-        :return: Data with Temperature and Humidity
-        """
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
-        if humidity is not None and temperature is not None:
-            return {"Temp": '{0:0.1f}*C'.format(temperature), "Humidity": '{0:0.1f}%'.format(humidity)}
-            self.humidity = humidity
-            self.temperature = temperature
+        self.hcsr04_init(21, 20)
+        self.get_data_from_dht11(4)
+        if self.response_data["dht_11"]["Temp"]:
+            self.response_data["dht_11"]["status"] = "active"
+            self.response_data["hcsr_04"]["status"] = "active"
         else:
-            return {"Temp": None, "Humidity": None}
+            self.response_data["error_code"] = "initialization_failed"
 
-    def hcSr04_Init(self, TRIG=21, ECHO=20):
+        return self.response_data
+
+    def get_state_of_all_sensors(self):
+        self.initialize_all_sensors()
+        return self.response_data
+
+    def get_data_from_dht11(self, pin_id):
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin_id)
+
+        if humidity is not None and temperature is not None:
+            self.response_data.update(
+                {"dht_11": {"Temp": '{0:0.1f}*C'.format(temperature), "Humidity": '{0:0.1f}%'.format(humidity)}})
+        else:
+            self.response_data.update({"data": {"Temp": None, "Humidity": None}})
+
+    def hcsr04_init(self, trig, echo):
         """
         Init the HCSR04 sensor
         """
@@ -48,35 +44,27 @@ class SensorAPI:
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         # Set direction
-        GPIO.setup(TRIG, GPIO.OUT)
-        GPIO.setup(ECHO, GPIO.IN)
+        GPIO.setup(trig, GPIO.OUT)
+        GPIO.setup(echo, GPIO.IN)
 
-    def getDistance_Of_HcSr04Init(self, pinTrig=21, pinEcho=20):
-        """
-        Function to get the distance form opstacle to our sensor
-
-        :param pinEcho: pin to Trigered, pin to get Data Echo
-        :return: Distance from opstacle
-        """
+    def hcsr04_init_get_distance(self, trig, echo):
         # Pulse to start measure with HC-SR04
-        GPIO.output(pinTrig, True)
+        GPIO.output(trig, True)
         time.sleep(0.00001)
-        GPIO.output(pinTrig, False)
+        GPIO.output(trig, False)
         # Wait for HIGH on ECHO
-        while GPIO.input(pinEcho) == 0:
+        while GPIO.input(echo) == 0:
             pulse_start = time.time()
         # wait for LOW again
-        while GPIO.input(pinEcho) == 1:
+        while GPIO.input(echo) == 1:
             pulse_end = time.time()
-        signalDelay = pulse_end - pulse_start
+        signal_delay = pulse_end - pulse_start
         # divider for uS to  s
-        constDivider = 1000000 / 58
-        distance = int(signalDelay * constDivider)
-        return {"Distance": distance}
+        const_divider = 1000000 / 58
+        distance = int(signal_delay * const_divider)
+        self.response_data["hcsr_04"]["distance"] = str(distance)
 
-    def getData(self):
-        tim = datetime.datetime.now()
-        JSon = {"data": {"Time": "{}".format(tim)}}
-        JSon["data"].update(self.getDistance_Of_HcSr04Init())
-        JSon["data"].update(self.getTempHumi())
-        return JSon
+    def get_data_from_sensor(self, sensor_id):
+        self.hcsr04_init_get_distance(21, 20)
+        self.get_data_from_dht11(4)
+        return {self.response_data[sensor_id]}
